6bb2b2429c6bd9cca886f3d8bcb497271f5cf331
baderbintahir <baderbintahir@gmail.com>
2020-02-12 12:25:08 +0500

    test

---
+++ test.js
@@ -0,0 +1,65 @@
+// for(var i = 0; i < 5; i++){
+//     setTimeout(() => {
+//         // console.log(i);
+//     }, i * 1000);
+// }
+
+// console.log(i);
+
+// for(i = 0; i < 5; i++){
+//     setTimeout(() => {
+//         console.log(i);
+//     }, i * 1000);
+// }
+
+// console.log(i);
+
+// x();
+// function x(){
+//     console.log('b da swag!');
+//     y();
+//     function y(){     
+//         z();   
+//         function z(){
+//             b = 8;
+//             console.log('b is the most gangsta shit');
+//         }        
+//     }    
+// }
+// console.log(b);
+
+
+
+// async function x(){
+//     console.log('Hello World');
+//     return 12;
+// }
+// console.log(x());
+
+// var x = 'asdf';
+// a = x[0];
+// b = [x.length-1];
+// x[0] = b;
+// x[x.length - 1] = a;
+// console.log(x);
+
+
+// var e = 5;
+// console.log(e);
+// try {
+//     throw 'Hey Hey Hey!';
+// } catch (e) {
+//     console.log(e);
+// }
+// console.log(e);
+
+
+
+// var a = 2;
+
+// function x(){
+//     console.log(a);
+// }
+
+// x();
+// console.log(typeof x);
